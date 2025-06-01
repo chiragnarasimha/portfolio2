e9348e528223e9e3df15c1f4582473a904d012b3
chiragnarasimha <chiragnarasimha@gmail.com>
2025-06-01 22:55:20 +1000

    Started on the guess Game

---
+++ src/games/guessTheLetterGame/GuessGame.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import styles from './GuessGame.module.css'
+
+const GuessGame = () => {
+    return (
+        <div className={styles.text}>This is the guess Game</div>
+    )
+}
+export default GuessGame
