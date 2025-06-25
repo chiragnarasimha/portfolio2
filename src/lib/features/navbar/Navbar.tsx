717f29757c22e67fbaa5b1ec00bdd55882a7acd5
chiragnarasimha <chiragnarasimha@gmail.com>
2025-06-25 19:53:46 +1000

    Remove debug text from page component and update NavMenuButton import path

--- src/lib/features/navbar/Navbar.tsx
+++ src/lib/features/navbar/Navbar.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import styles from "./Navbar.module.css";
 import { ChiragLogo } from "@/lib/assets/images/SvgComponents";
 import DarkModeToggle from "@/lib/features/navbar/darkmode/DarkModeToggle";
-import NavMenuButton from "@/lib/features/navbar/NavMenuButton";
+import NavMenuButton from "@/lib/features/navbar/menu/NavMenuButton";
 
 const Navbar = () => {
   return (
