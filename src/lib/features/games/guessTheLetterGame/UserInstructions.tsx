25a895fca48adc5ae0abb844266973f27ab7395f
chiragnarasimha <chiragnarasimha@gmail.com>
2025-07-06 14:08:19 +1000

    Remove unused React imports from UserInstructions component

--- src/lib/features/games/guessTheLetterGame/UserInstructions.tsx
+++ src/lib/features/games/guessTheLetterGame/UserInstructions.tsx
@@ -1,5 +1,4 @@
 "use client";
-import React, { useState, useEffect, useMemo, useCallback } from "react";
 import styles from "./UserInstructions.module.css";
 import { useAppSelector } from "@/lib/hooks";
 import {
